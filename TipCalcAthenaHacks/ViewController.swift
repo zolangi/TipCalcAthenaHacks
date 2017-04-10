5c3f130558e9ab7ebe3b2f536b4e7067681624f4
zolangi <zolangiramirez@gmail.com>
2017-04-09 19:40:25 -0700

    Third commit

--- TipCalcAthenaHacks/ViewController.swift
+++ TipCalcAthenaHacks/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        if let billAmount = Double(billAmountField.text!) {
+            var tipPercentage = 0.0
+            
+            switch tipSelector.selectedSegmentIndex {
+            case 0:
+                tipPercentage = 0.10
+            case 1:
+                tipPercentage = 0.15
+            case 2:
+                tipPercentage = 0.20
+            default:
+                break
+            }
+            
+            let roundedBillAmount = round(100 * billAmount) / 100
+            let tipAmount = roundedBillAmount * tipPercentage
+            let roundedTipAmount = round(100*tipAmount)/100
+            let totalAmount = roundedBillAmount + roundedTipAmount
+            
+            if (!billAmountField.isEditing) {
+                billAmountField.text = String(format: "%.2f", roundedBillAmount)
+            }
+            tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+            totalAmountField.text = String(format: "%.2f", totalAmount)
+        } else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
